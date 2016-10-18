3f0aa83f8249a27744300f9481003bb7013260d5
Jean-Baptiste Alleaume <jbanety@gmail.com>
2016-10-18 19:12:47 +0200

    correction password_hash

--- src/Web.php
+++ src/Web.php
@@ -947,7 +947,7 @@ class Web extends AbstractWebApplication implements ContainerAwareInterface {
                   ->where($db->quoteName('uastring') . ' = ' . $db->quote($cookieName));
         }
 
-        $hashed_token = password_hash($token, PASSWORD_BCRYPT, null);
+        $hashed_token = password_hash($token, PASSWORD_BCRYPT);
         $query->set($db->quoteName('token') . ' = ' . $db->quote($hashed_token));
         $db->setQuery($query)
                  ->execute();
